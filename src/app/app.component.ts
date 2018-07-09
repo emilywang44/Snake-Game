971cb45a5b2bd89f4f3d5f6729706d2318d7f1dd
WAE1PLY <fixed-term.emily.wang2@us.bosch.com>
2018-07-09 15:15:19 -0400

    snake moves automatically

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,46 +18,100 @@ import { HostListener } from '@angular/core';
 
 export class AppComponent  { 
 
-  @HostListener('window:keydowm', ['$event'])
-  handleKeyboardEvent(event: KeyboardEvent){
-    switch(event.keyCode){
-      case 37:
-        //left
-        moveSnake("left");
+
+}
+
+var currDir = "left";
+
+function moveSnake() {
+
+  var rect = document.getElementById("snake").getBoundingClientRect();
+  switch(currDir){
+
+    case "left":
+      if (currDir === "left")
+        document.getElementById("snake").style.left = (rect.left - 20) + "px";
+      else if (currDir === "up" || currDir === "down")    
+      break;
+
+    case "right":
+      if (currDir === "right")
+        document.getElementById("snake").style.left = (rect.left + 20) + "px";
+      break;
+
+    case "up":
+      if (currDir === "up")
+        document.getElementById("snake").style.top = (rect.top - 20) + "px";
+      else if (currDir === "left")
+
+      break;
+
+    case "down":
+      if(currDir === "down")
+        document.getElementById("snake").style.top = (rect.top + 20) + "px";
+      break;
+
+      default:
         break;
-      case 38:
-        //up
-        moveSnake("up");
-        break;
-      case 39:
-        //right
-        moveSnake("right");
-        break;
-      case 40:
-        //down
-        moveSnake("down");
-        break;
-    }
   }
 
 }
 
-function moveSnake(dir : String) {
+function generateFood(){
 
-  var rect = document.getElementById("snake").getBoundingClientRect();
-  switch(dir){
-    case "left":
-      document.getElementById("snake").style.left = (rect.left - 10) + "px";
-      break;
-    case "right":
-      document.getElementById("snake").style.left = (rect.left + 10) + "px";
-      break;
-    case "up":
-      document.getElementById("snake").style.top = (rect.top - 10) + "px";
-      break;
-    case "down":
-      document.getElementById("snake").style.top = (rect.top + 10) + "px";
-      break;
+  //generate a random number between grid min width and grid max width
+  var newX = Math.floor((Math.random() * (grid.x + grid.width)) + grid.x);
+
+  //generate a random number between grid min height and grid max height
+  var newY = Math.floor((Math.random() * (grid.y + grid.height)) + grid.y);
+
+  food.x = newX;
+  food.y = newY;
+
+}
+
+var snake = {
+  length: 5,
+  headDir: "right",
+  tailDir: "right",
+  bend: "",
+  x: 0,
+  y: 0
+};
+
+var food = {
+  x: 0,
+  y: 0
+};
+
+var grid = {
+  x: 0, 
+  y: 0,
+  height: 100, 
+  width: 100
+};
+
+window.onload = function() {
+  this.setInterval(moveSnake, 100);
+  window.onkeydown = function(e){
+    switch(e.keyCode){
+      case 37:
+        //left
+        currDir = "left";
+        break;
+      case 38:
+        //up
+        currDir = "up";
+        break;
+      case 39:
+        //right
+        currDir = "right";
+        break;
+      case 40:
+        //down
+        currDir = "down";
+        break;
+    }
+    
   }
-
 }
